704482e18c38d3205a4647588e486cc6d856fe33
Cesar <luiscesar.munoz.cervantes.upiit@gmail.com>
2023-04-11 22:33:17 -0600

    7 y al parecer todo era una pavada

--- pages/aire/aire.js
+++ pages/aire/aire.js
@@ -1,3 +1,10 @@
+import Link from "next/link";
+import styles from './styles.module.css'
+
 export default function Aire(){
-  return <h1>Aire acondicionado</h1>
+  return <>
+   <h1>Aire acondicionado
+  </h1>
+  <Link href='/aire/miniSplit' className={styles.link_mini}>minisplit</Link>
+  </>
 }
---
+++ pages/aire/miniSplit.js
@@ -0,0 +1,3 @@
+export default function Mini (){
+  return <h1>mini</h1>
+}
