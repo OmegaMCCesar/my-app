f31384a4b345617b594435808462bb7067bd44d9
Cesar <luiscesar.munoz.cervantes.upiit@gmail.com>
2023-04-13 02:57:21 -0600

    ya quedo

--- components/marcas.jsx
+++ components/marcas.jsx
@@ -1,8 +1,16 @@
 import styles from '../styles/marcas.module.css'
-const lg = '/LG.ico';
-const mabe = '/mabe.ico'
+const LG = '/LG.ico';
+const MABE = '/mabe.ico';
+const WHIRLPOOL = '/Whirlpool.ico';
+const DAEWOO = '/daewoo.ico';
+const EASY = '/easy.ico';
+const SAMSUNG = '/Samsung.ico';
+const SUBZERO = '/subzero.ico';
+const TORREY = '/Torrey.ico';
 
-const marcas = [lg, mabe]
+
+
+const marcas = [LG, MABE, WHIRLPOOL,DAEWOO,EASY,SAMSUNG,SUBZERO,TORREY]
 
 const Marcas = () => {
   return <div className={styles.container}>
