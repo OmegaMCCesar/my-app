cfdd2a40161fb82abe82a459a2957be68b79d161
Cesar <luiscesar.munoz.cervantes.upiit@gmail.com>
2023-04-25 23:19:16 -0600

    muy pequeño cambio en css

--- components/cuerpo.jsx
+++ components/cuerpo.jsx
@@ -3,7 +3,7 @@ import styles from '../styles/cuerpo.module.css';
 const Cuerpo = () => {
   return <div className={styles.anuncios}>
     <h1>Garantia por escrito</h1>
-    <h2>6 meses de garantia en el trabajo realizado, por escrito.</h2>
+    <h2>6 meses de garantia en el trabajo realizado.</h2>
   </div>
 }
 
