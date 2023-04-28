7c7e78da77e075d0399878d4433f5b1f4ed3b3dc
Cesar <luiscesar.munoz.cervantes.upiit@gmail.com>
2023-04-27 23:40:33 -0600

    actualizacion relevante 6

--- components/cuerpo.jsx
+++ components/cuerpo.jsx
@@ -1,9 +1,22 @@
 import styles from '../styles/cuerpo.module.css';
 
+
+
 const Cuerpo = () => {
   return <div className={styles.anuncios}>
-    <h1>Garantia por escrito</h1>
-    <h2>6 meses de garantia en el trabajo realizado.</h2>
+    <div className={styles.garantia}>
+      <h1 className={styles.title}>Garantia por escrito</h1>
+      <h2 className={styles.text}> 6 meses de garantia en el trabajo realizado. </h2>
+    </div>
+    <div className={styles.medios_pago}>
+      <h1>Metodos de pago</h1>
+      <ul>
+        <li>Efectivo<img src='./efectivo.png' alt='efectivo' className={styles.pago} /></li>
+        <li>Transferencia<img src='./transferencia.png' alt='trasnferencia' className={styles.pago} /></li>
+        <li>Tarjeta Credito/Debito <img src='./masCard.png' alt='mastercard' className={styles.pago}/><img src='./visa.png' alt='visa' className={styles.pago}/></li>
+        <li>Vales de despensa<img src='./vale.jpg' alt='vales' className={styles.pago} /></li>
+      </ul>
+    </div>
   </div>
 }
 
